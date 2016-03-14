fa1a4e6603db0886ba899fce822f1572c7b86294
Mirza Hasanbasic <lekroya@gmail.com>
2016-03-14 15:56:44 +0100

    Hippo is alive

--- data/basicIT.m
+++ data/basicIT.m
@@ -1,15 +1,15 @@
-function [left, right] = basicIT(x)
+function [hippoBox] = basicIT(x)
 
 load(x);
 
 left = [];
-right = [];
-tmpmin = 1;
-tmpmax = 0;
-printvar = [0 0 0 0];
-counter0 = 0;
+% right = [];
+% tmpmin = 1;
+% tmpmax = 0;
+% printvar = [0 0 0 0];
+% counter0 = 0;
 counter1 = 0;
-counter2 = 0;
+% counter2 = 0;
  for i = 1:256
      for j = 1:256
          for k = 1:256
@@ -24,24 +24,23 @@ counter2 = 0;
                  left = [left; [i j k];];
                  counter1 = counter1 + 1;
              end
-             if segmentation(i, j, k) == 2;
-                 %printvar = [1, i, j ,k];
-                 %disp(printvar);
-                 right = [right; [i j k];];
-                 counter2 = counter2 + 1;
-             end
+%              if segmentation(i, j, k) == 2;
+%                  %printvar = [1, i, j ,k];
+%                  %disp(printvar);
+%                  right = [right; [i j k];];
+%                  counter2 = counter2 + 1;
+%              end
          end
      end
  end
- 
-LeftVal = [];
-
+%LeftVal = [];
 for i = 1:length(left) 
     coordi = left(i,1);
     coordj = left(i,2);
     coordk = left(i,3);
     LeftVal = [LeftVal; mri(coordi, coordj, coordk);];
 end
+
 % Forrest:
 % venstrenede min i, min j, min k
 % venstreoppe min i, max j, min k
@@ -53,6 +52,7 @@ end
 % venstreoppe min i, max j, max k
 % højrenede   max i, min j, max k
 % højreoppe   max i, max j, max k
+
 minI = min(left(:,1));
 minJ = min(left(:,2));
 minK = min(left(:,3));
@@ -60,10 +60,17 @@ maxI = max(left(:,1));
 maxJ = max(left(:,2));
 maxK = max(left(:,3));
 
+hippoBox = NaN((maxI - minI+1), (maxJ - minJ+1), (maxK - minK+1), 'double');
+
+for i = 1:length(left)
+    tmp = mri(left(i,1),left(i,2),left(i,3));
+    hippoBox(left(i,1)+1-minI, left(i,2)+1-minJ, left(i,3)+1-minK) = tmp;
+end
+
+
 
 
 %billeder
-
+%imgBox = imagesc(hippoBox(:,:,15));
 %img2D = imagesc(imfuse(mri(:,:,160),segmentation(:,:,160),'blend'));
-
 
--- data/main.m
+++ data/main.m
@@ -1,2 +1,25 @@
-[left, right] = basicIT('1.mat');
+test1 = LeftHippoMatrix('1.mat');
 
+grayScaleLevels = 256;
+glcm = zeros(grayScaleLevels,grayScaleLevels);
+offset = [0 1; -1 1; -1 0; -1 -1];
+warning('off','all')
+for i = 1:size(test1,3);
+     [tmp, tmp1] = graycomatrix(test1(:,:,i),'Offset', [0 1], 'NumLevels',grayScaleLevels,'GrayLimits',[]);
+     glcm = glcm + tmp;
+     [tmp, tmp1] = graycomatrix(test1(:,:,i),'Offset', [-1 1], 'NumLevels',grayScaleLevels,'GrayLimits',[]);
+     glcm = glcm + tmp;
+     [tmp, tmp1] = graycomatrix(test1(:,:,i),'Offset', [-1 0], 'NumLevels',grayScaleLevels,'GrayLimits',[]);
+     glcm = glcm + tmp;
+     [tmp, tmp1] = graycomatrix(test1(:,:,i),'Offset', [-1 -1], 'NumLevels',grayScaleLevels,'GrayLimits',[]);
+     glcm = glcm + tmp;
+end
+
+% glcm1 = zeros(grayScaleLevels,grayScaleLevels);
+% for i = 1:size(test1,3);
+%      [tmp, tmp1] = graycomatrix(test1(:,:,i),'Offset', [-1 0], 'NumLevels',grayScaleLevels,'GrayLimits',[]);
+%      glcm1 = glcm1 + tmp;
+% end
+warning('on','all')
+glcm(glcm == 0) = NaN;
+imgwhatevs = surf(glcm);
