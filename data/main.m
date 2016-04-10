178b2a518208195ba180a9f2fb8775469c4763bf
Mirza Hasanbasic <lekroya@gmail.com>
2016-04-10 21:53:51 +0200

    latex til midtvej

--- data/main.m
+++ data/main.m
@@ -1,4 +1,4 @@
-test1 = LeftHippoMatrix('1.mat');
+test1 = LeftHippoMatrix('2.mat');
 
 grayScaleLevels = 256;
 glcm = zeros(grayScaleLevels,grayScaleLevels);
@@ -30,3 +30,16 @@ imgwhatevs = surf(glcm);
 %Machine Learning
 %Machine Learning 3D
 %Forskel i slicing på 2D - Kun hvis tid
+
+% 
+% img2D = imagesc(imfuse(mri(:,:,160),segmentation(:,:,160),'blend'));
+% 
+% img2D2 = imagesc(mri(:,160,:), 1,3);
+% 
+% [temp1, temp2, temp3] = mri(100,:,:);
+% 
+% S = permute(mri, [2 3 1]);
+% 
+% img2D2 = imagesc(S(:,:,150));
+
+
