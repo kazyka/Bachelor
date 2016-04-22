5946d8c4af8b3084a2b4434411e2db8510f85fbf
Bjorn123 <Mathiasrytter@gmail.com>
2016-04-22 12:14:30 +0200

    C_xPlusy fixet

--- matlab/C_xplusy.m
+++ matlab/C_xplusy.m
@@ -1,6 +1,12 @@
 function [C_xPy] = C_xplusy(my_glcm, k)
-
+N = 256;
 C_xPy = 0;
-for i=2:k
-    C_xPy = C_xPy + my_glcm(i-1,k-i+1);
+if k <= N
+    for i=2:k
+            C_xPy = C_xPy + my_glcm(i-1,k-i+1);
+    end
+else
+   for i = k-N:N
+        C_xPy = C_xPy + my_glcm(i,k-i);
+   end
 end
