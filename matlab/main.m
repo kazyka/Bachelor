9b943e71bae23579839476472ed54d53fe00ec6e
Bjorn123 <Mathiasrytter@gmail.com>
2016-06-03 01:47:15 +0200

    took forever

--- matlab/main.m
+++ matlab/main.m
@@ -17,14 +17,14 @@ addpath(fullfile(pwd,'data'));
 %KØR KUN HVIS DATA IKKE ER BLEVET KØRT FØR; ELLERS LOAD
 %HVIS UDREGNINGSMETODEN ER ÆNDRET; SÅ SKAL DEN KØRES IGEN
 %Kalder GLCM2D -> (glcm2dx,glcm2dy,glcm2dz) -> GLCM Toolbox
-[data_Derivations2D, data_Derivations3D] = datacalculation(1, 2, 'left', 'normalize', 'noterode');
-[data_Derivations2D, data_Derivations3D] = datacalculation(1, 2, 'left', 'normalize', 'erode');
-[data_Derivations2D, data_Derivations3D] = datacalculation(1, 2, 'left', 'normal', 'noterode');
-[data_Derivations2D, data_Derivations3D] = datacalculation(1, 2, 'left', 'normal', 'erode');
-[data_Derivations2D, data_Derivations3D] = datacalculation(1, 2, 'right', 'normalize', 'noterode');
-[data_Derivations2D, data_Derivations3D] = datacalculation(1, 2, 'right', 'normalize', 'erode');
-[data_Derivations2D, data_Derivations3D] = datacalculation(1, 2, 'right', 'normal', 'noterode');
-[data_Derivations2D, data_Derivations3D] = datacalculation(1, 2, 'right', 'normal', 'erode');
+[data_Derivations2D, data_Derivations3D] = datacalculation(1, 100, 'left', 'normalize', 'noterode');
+[data_Derivations2D, data_Derivations3D] = datacalculation(1, 100, 'left', 'normalize', 'erode');
+[data_Derivations2D, data_Derivations3D] = datacalculation(1, 100, 'left', 'normal', 'noterode');
+[data_Derivations2D, data_Derivations3D] = datacalculation(1, 100, 'left', 'normal', 'erode');
+[data_Derivations2D, data_Derivations3D] = datacalculation(1, 100, 'right', 'normalize', 'noterode');
+[data_Derivations2D, data_Derivations3D] = datacalculation(1, 100, 'right', 'normalize', 'erode');
+[data_Derivations2D, data_Derivations3D] = datacalculation(1, 100, 'right', 'normal', 'noterode');
+[data_Derivations2D, data_Derivations3D] = datacalculation(1, 100, 'right', 'normal', 'erode');
 %[data_DerivationsErode] = dataexecutionErode(1,100);
 %[data123] = dataexecutionTestErode(1,100);
 %[DataTest] = specialLoader(100,1);
