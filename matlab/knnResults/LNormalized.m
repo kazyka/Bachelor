5a66be5262c8be7e6d04e27e451711cea2df52ee
Bjorn123 <Mathiasrytter@gmail.com>
2016-06-05 19:21:20 +0200

    more test, more test more test

---
+++ matlab/knnResults/LNormalized.m
@@ -0,0 +1,35 @@
+NumberOfPatients =100;
+[DATA2D, DATA3D] = dataloader(NumberOfPatients , 1, 'left', 'normalize', 'noterode');
+COKTrainData = DATA2D(1:50,:);
+ADKTrainData = DATA2D(51:100,:);
+COKTrainData3D = DATA3D(1:50,:);
+ADKTrainData3D = DATA3D(51:100,:);
+%randomly sample from data, without replacment
+[sampledCOKTrainData2D, idxCO2D] = datasample(COKTrainData, NumberOfPatients/2, 'Replace', false);
+[sampledADKTrainData2D, idxAD2D] = datasample(ADKTrainData, NumberOfPatients/2, 'Replace', false);
+[sampledCOKTrainData3D, idxCO3D] = datasample(COKTrainData3D, NumberOfPatients/2, 'Replace', false);
+[sampledADKTrainData3D, idxAD3D] = datasample(ADKTrainData3D, NumberOfPatients/2, 'Replace', false);
+sortedData2D = cell(100,1);
+sortedData3D = cell(100,1);
+for i=1:10
+    for j =1:5
+        sortedData2D{j+10*(i-1)} = sampledCOKTrainData2D{j+5*(i-1)};
+        sortedData2D{5+j+10*(i-1)} = sampledADKTrainData2D{j+5*(i-1)};
+        sortedData3D{j+10*(i-1)} = sampledCOKTrainData3D{j+5*(i-1)};
+        sortedData3D{5+j+10*(i-1)} = sampledADKTrainData3D{j+5*(i-1)};
+    end
+end
+%Dataset form , patients(100) X glcms(9 2d, 13 3d) X features(13) X
+%distances(10)
+dataSorted2D = forwardFeatureDataSort(sortedData2D,NumberOfPatients);
+dataSorted3D = forwardFeatureDataSort(sortedData3D,NumberOfPatients);
+%Find best and accuracy K for all 4 datasets
+toD = cell(10,1);
+treD = cell(10,1);
+for i=1:10
+    toD{i} = ForwardSelection(dataSorted2D,i,10);
+    treD{i} = ForwardSelection(dataSorted3D,i,10);
+    fprintf('FS done for k = %d  done \n',i)
+end
+save(sprintf('knnResults/notErodeLeft2DNormalized'), 'toD');
+save(sprintf('knnResults/notErodeLeft3DNormalized'), 'treD');
